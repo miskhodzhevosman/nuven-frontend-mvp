45ae5ee2f4447dd241c271a5253a90dfa7df3762
miskhodzhevosman <miskhodzhev.osman@gmail.com>
2026-07-25 04:41:25 +0300

    fix auth and add new host

--- src/composables/useApi.js
+++ src/composables/useApi.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://147.45.230.130:8000/api/',
+  baseURL: 'https://api.nuven.space/api/',
   headers: {
     'Content-Type': 'application/json'
   }
@@ -78,7 +78,7 @@ api.interceptors.response.use(
       }
       
       // Используем axios напрямую, чтобы избежать цикла
-      const response = await axios.post('http://147.45.230.130:8000/api/token/refresh/', {
+      const response = await axios.post('https://api.nuven.space/api/token/refresh/', {
         refresh: refreshToken
       })
       
